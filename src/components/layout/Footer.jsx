d4a67cf5edc19a908f55a0579b268a31b9cdd21f
sofiver2023 <verdejasofia4@gmail.com>
2026-07-06 19:45:16 -0300

    feat: mejorar Footer con íconos de redes sociales y fix de caracteres

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,9 @@
-import { Box, Typography, Grid } from "@mui/material";
+import { Box, Typography, Grid, IconButton } from "@mui/material";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 function Footer() {
   return (
@@ -13,7 +17,7 @@ function Footer() {
       }}
     >
       <Grid container spacing={4}>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} md={4}>
           <Box sx={{ display: "flex", alignItems: "center", gap: 1, marginBottom: 1 }}>
             <PeopleAltIcon />
             <Typography variant="h6">Gestión de Clientes</Typography>
@@ -26,7 +30,7 @@ function Footer() {
           </Typography>
         </Grid>
 
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} md={4}>
           <Typography variant="subtitle1" sx={{ fontWeight: "bold", marginBottom: 1 }}>
             INTEGRANTES
           </Typography>
@@ -36,16 +40,29 @@ function Footer() {
           <Typography variant="body2" color="grey.400">Victoria Valentina Valencia</Typography>
           <Typography variant="body2" color="grey.400">Glenda Sofia Verdeja</Typography>
         </Grid>
+
+        <Grid item xs={12} md={4}>
+          <Typography variant="subtitle1" sx={{ fontWeight: "bold", marginBottom: 1 }}>
+            REDES SOCIALES
+          </Typography>
+          <Box sx={{ display: "flex", gap: 1 }}>
+            <IconButton color="inherit" href="https://facebook.com" target="_blank">
+              <FacebookIcon />
+            </IconButton>
+            <IconButton color="inherit" href="https://instagram.com" target="_blank">
+              <InstagramIcon />
+            </IconButton>
+            <IconButton color="inherit" href="https://linkedin.com" target="_blank">
+              <LinkedInIcon />
+            </IconButton>
+            <IconButton color="inherit" href="https://github.com/sofiver2023/pv_tp_integrador_grupo4" target="_blank">
+              <GitHubIcon />
+            </IconButton>
+          </Box>
+        </Grid>
       </Grid>
 
-      <Box
-        sx={{
-          borderTop: "1px solid grey",
-          marginTop: 3,
-          paddingTop: 2,
-          textAlign: "center",
-        }}
-      >
+      <Box sx={{ borderTop: "1px solid grey", marginTop: 3, paddingTop: 2, textAlign: "center" }}>
         <Typography variant="body2" color="grey.400">
           © 2026 Proyecto Final APU - Comisión 3 Grupo 4. Todos los derechos reservados.
         </Typography>
