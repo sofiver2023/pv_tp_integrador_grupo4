bc9568340c9fe2748981f0846e02b2d2d36bdce8
Jonatan Garcia <sacha.255555@gmail.com>
2026-07-06 23:43:22 -0300

    chore/fix(ui): Corregir Footer luego de merge de rama feature/mejoras-esteticas

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,6 +4,7 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 function Footer() {
   return (
